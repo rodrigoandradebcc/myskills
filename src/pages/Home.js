50c5424357f8e4cfc99336b5459032e0e010a406
Rod Andrade <randrade@brbatel.com.br>
2021-09-03 12:44:24 -0300

    refactor: using stylesheet

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,58 @@
 import React from 'react';
 
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, SafeAreaView, TextInput, Platform, TouchableOpacity, StatusBar} from 'react-native';
 
 export function Home() {
   return (
     <>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        <Text>React Native</Text>
-      </View>
+      <StatusBar translucent backgroundColor="transparent" />
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.title}>Welcome Rodrigo</Text>
 
-      <Text
-        style={{
-          alignSelf: 'center',
-          marginBottom: 20,
-        }}>
-        Ignite
-      </Text>
+        <TextInput style={styles.input} placeholder="New skill" placeholderTextColor="#555"/>
+
+        <TouchableOpacity style={styles.button} activeOpacity={.7}>
+          <Text style={styles.buttonText}>Add</Text>
+        </TouchableOpacity>
+
+        <Text  style={[styles.title, {marginTop: 50}]}>
+          My Skills
+        </Text>
+      </SafeAreaView>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121015',
+    paddingVertical: 70,
+    paddingHorizontal: 20,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  input: {
+    backgroundColor: '#1f1e25',
+    color: '#fff',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30, 
+    borderRadius: 7
+  },
+  button: {
+    backgroundColor: '#a370f7',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 17,
+    fontWeight: 'bold',
+  }
+});
