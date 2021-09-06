5a76e47dab4084a2e2992ebe47d2be325dc83d83
Rod Andrade <randrade@brbatel.com.br>
2021-09-06 16:11:22 -0300

    refactor: immutability react native

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,49 @@
-import React from 'react';
-
-import {View, Text, StyleSheet, SafeAreaView, TextInput, Platform, TouchableOpacity, StatusBar} from 'react-native';
+import React, {useState} from 'react';
+import {
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 
 export function Home() {
+  const [newSkill, setNewSkill] = useState('');
+  const [mySkills, setMySkills] = useState([]);
+
+  function handleAddNewSkill(skill) {
+    setMySkills(oldState => [...oldState, newSkill]);
+  }
+
   return (
     <>
       <StatusBar translucent backgroundColor="transparent" />
       <SafeAreaView style={styles.container}>
         <Text style={styles.title}>Welcome Rodrigo</Text>
 
-        <TextInput style={styles.input} placeholder="New skill" placeholderTextColor="#555"/>
+        <TextInput
+          style={styles.input}
+          placeholder="New skill"
+          placeholderTextColor="#555"
+          onChangeText={setNewSkill}
+        />
 
-        <TouchableOpacity style={styles.button} activeOpacity={.7}>
+        <TouchableOpacity
+          style={styles.button}
+          activeOpacity={0.7}
+          onPress={handleAddNewSkill}>
           <Text style={styles.buttonText}>Add</Text>
         </TouchableOpacity>
 
-        <Text  style={[styles.title, {marginTop: 50}]}>
-          My Skills
-        </Text>
+        <Text style={[styles.title, {marginVertical: 50}]}>My Skills</Text>
+
+        {mySkills.map((skill, index) => (
+          <TouchableOpacity style={styles.buttonSkill} key={index}>
+            <Text style={[styles.textSkill]}>{skill}</Text>
+          </TouchableOpacity>
+        ))}
       </SafeAreaView>
     </>
   );
@@ -40,19 +66,31 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 18,
     padding: Platform.OS === 'ios' ? 15 : 10,
-    marginTop: 30, 
-    borderRadius: 7
+    marginTop: 30,
+    borderRadius: 7,
   },
   button: {
     backgroundColor: '#a370f7',
     padding: 15,
     borderRadius: 7,
     alignItems: 'center',
-    marginTop: 20
+    marginTop: 20,
   },
   buttonText: {
     color: '#fff',
     fontSize: 17,
     fontWeight: 'bold',
-  }
+  },
+  buttonSkill: {
+    backgroundColor: '#1f1e25',
+    padding: 15,
+    borderRadius: 50,
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  textSkill: {
+    color: '#fff',
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
 });
